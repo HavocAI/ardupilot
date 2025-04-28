22b74a0e1396acee88bc933e06956325ac32ee53
Paul Soucy <paul.soucy@havocai.com>
2025-04-28 17:23:15 -0400

    wip

--- libraries/AP_IrisOrca/AP_IrisOrcaModbus.h
+++ libraries/AP_IrisOrca/AP_IrisOrcaModbus.h
@@ -10,6 +10,80 @@
 
 #define MODBUS_MAX_MSG_LEN 64
 
+
+namespace orca {
+    // message addresses
+    enum MsgAddress : uint8_t {
+        BUS_MASTER = 0x00,
+        DEVICE = 0x01
+    };
+
+    // function codes
+    enum FunctionCode : uint8_t {
+        WRITE_REGISTER = 0x06,
+        WRITE_MULTIPLE_REGISTERS = 0x10,
+        MOTOR_COMMAND_STREAM = 0x64,
+        MOTOR_READ_STREAM = 0x68
+    };
+
+    // sub codes for MOTOR_COMMAND_STREAM
+    enum MotorCommandStreamSubCode : uint8_t {
+        FORCE_CONTROL_STREAM = 0x1C,
+        POSITION_CONTROL_STREAM = 0x1E,
+        SLEEP_DATA_STREAM = 0x00 // sleep data stream is everything else
+    };
+
+    // registers
+    enum Register : uint16_t {
+        CTRL_REG_0 = 0,
+        CTRL_REG_2 = 2,
+        CTRL_REG_3 = 3,
+        CTRL_REG_4 = 4,
+        POS_CMD = 30,
+        POS_CMD_H = 31,
+        PC_PGAIN = 133,
+        PC_IGAIN = 134,
+        PC_DVGAIN = 135,
+        PC_DEGAIN = 136,
+        PC_FSATU = 137,
+        PC_FSATU_H = 138,
+        ZERO_MODE = 171,
+        AUTO_ZERO_FORCE_N = 172,
+        AUTO_ZERO_EXIT_MODE = 173,
+        SERIAL_NUMBER_LOW = 406,
+        SERIAL_NUMBER_HIGH = 407,
+        MAJOR_VERSION = 408,
+        RELEASE_STATE = 409,
+        REVISION_NUMBER = 410,
+    };
+
+    enum class OperatingMode : uint8_t {
+        SLEEP = 1,
+        FORCE = 2,
+        POSITION = 3,
+        HAPTIC = 4,
+        KINEMATIC = 5,
+        AUTO_ZERO = 55,
+    };
+
+    // motor control states
+    enum class MotorControlState : uint8_t {
+        CONFIGURING = 0,
+        AUTO_ZERO = 1,
+        POSITION_CONTROL = 2
+    };
+
+    struct ActuatorState {
+        OperatingMode mode;
+        uint32_t shaft_position;
+        uint32_t force_realized;
+        uint16_t power_consumed;
+        uint8_t temperature;
+        uint16_t voltage;
+        uint16_t errors;
+    };
+}
+
 void init_uart_for_modbus(AP_HAL::UARTDriver *uart);
 
 class AP_ModbusTransaction
@@ -20,7 +94,7 @@ public:
         uint8_t len;
     } Buffer;
 
-    AP_ModbusTransaction() = default;
+    AP_ModbusTransaction() {};
     AP_ModbusTransaction(AP_HAL::UARTDriver *uart);
     // CLASS_NO_COPY(AP_ModbusTransaction);
 
@@ -63,9 +137,9 @@ private:
 
 class WriteRegisterTransaction : public AP_ModbusTransaction {
     public:
-        WriteRegisterTransaction() = default;
+        WriteRegisterTransaction() {};
         WriteRegisterTransaction(AP_HAL::UARTDriver *uart, uint16_t reg_addr, uint16_t reg_value);
-        CLASS_NO_COPY(WriteRegisterTransaction);
+        // CLASS_NO_COPY(WriteRegisterTransaction);
     
     protected:
         virtual bool parse_response(uint8_t byte) override;
@@ -87,9 +161,9 @@ class WriteRegisterTransaction : public AP_ModbusTransaction {
 
 class ReadRegisterTransaction : public AP_ModbusTransaction {
     public:
-        ReadRegisterTransaction() = default;
+        ReadRegisterTransaction() {};
         ReadRegisterTransaction(AP_HAL::UARTDriver *uart, uint16_t reg_addr);
-        CLASS_NO_COPY(ReadRegisterTransaction);
+        // CLASS_NO_COPY(ReadRegisterTransaction);
     
         uint16_t reg_value() const { return _reg_value; }
 
